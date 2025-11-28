543c564c3d29e405a91de93bf0243a44c47ba5c3
Lakshmimathur5 <laxmikumari6690@gmail.com>
2025-11-28 23:40:34 +0530

    Daily practice

---
+++ DefineAndUseOfClass.java
@@ -0,0 +1,35 @@
+class Dog {
+    String Name;
+    String Breed;
+    String color;
+    int age;
+    public Dog(String Name, String Breed, String color, int age){
+        this.Name = Name;
+        this.Breed = Breed;
+        this.color = color;
+        this.age = age;
+
+    }
+    public String getName(){
+        return Name;
+    }
+    public String getColor(){
+        return color;
+    }
+    public String getBreed(){
+        return Breed;
+    }
+    public int getAge(){
+        return age;
+    }
+    @Override
+
+    public String toString(){
+        return "Name is:" +Name+ "\nBreed age and color are:" +Breed+ " " +age+ " " +color;
+    }
+    public static void main(String[] args){
+        Dog tuffy = new Dog("tuffy", "papillon", "black", 5 );
+        System.out.println(tuffy);
+
+    }
+}
